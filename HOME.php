1fd3376fae5dc13283efe676b157b2b3350c850e
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-08-11 21:19:35 +0800

    Create HOME.php

---
+++ HOME.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body bgcolor="pink">
+<head>
+	<title>HOME</title>
+</head>
+<body>
+	<center><h1>Bering, Reyner Paul's PHOTO GALLERY</h1></center>
+<br>
+<br>
+<br>
+<br>
+<br>
+	<nav><center>
+		<h3><a href="image1.html"> IMAGE 1 </a><br></h3>
+		<h3><a href="image2.html"> IMAGE 2 </a><br></h3>
+		<h3><a href="image3.html"> IMAGE 3 </a></h3>
+	</center></nav>
+</body>
+</html>
