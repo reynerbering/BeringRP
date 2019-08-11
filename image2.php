ccd68e3c8f66bb32a13366707c1c74be61226a2c
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-08-11 22:26:47 +0800

    Update image2.php

--- image2.php
+++ image2.php
@@ -5,6 +5,7 @@
 	<title>image2</title>
 </head>
 <body>
+<center><h1>Bering, Reyner Paul's PHOTO GALLERY</h1></center><center>
 <h1><center>YA DUDE</center></h1>
 
 <center><img src="image2.jpg" alt="image2" width="500" height="500"></center>
