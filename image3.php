af7f5654234497f3fbe9dcce861ab674f9044b29
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-08-11 22:26:56 +0800

    Update image3.php

--- image3.php
+++ image3.php
@@ -5,6 +5,7 @@
 	<title>image3</title>
 </head>
 <body>
+<center><h1>Bering, Reyner Paul's PHOTO GALLERY</h1></center><center>
 <h1><center>YA BOSS</center></h1>
 
 <center><img src="image3.jpg" alt="image3" width="600" height="850"></center>
