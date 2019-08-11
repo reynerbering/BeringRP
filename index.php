88a367d2575a5bfb9504848f735ff6def2b7e41e
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-08-11 22:01:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
 </head>
 <body>
 	<center><h1>Bering, Reyner Paul's PHOTO GALLERY</h1></center>
+	<center>
 <img src="IMAGE.gif">
+	</center>
 	<nav><center>
 		<h3><a href="image1.php"> IMAGE 1 </a><br></h3>
 		<h3><a href="image2.php"> IMAGE 2 </a><br></h3>
