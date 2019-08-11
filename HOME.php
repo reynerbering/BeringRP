e87e127782b64f8a1077734c6ce956aa943f5933
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-08-11 21:37:36 +0800

    Update HOME.php

--- HOME.php
+++ HOME.php
@@ -9,9 +9,9 @@
 	<center><h1>Bering, Reyner Paul's PHOTO GALLERY</h1></center>
 
 	
-		<h3><a href="image1.html"> IMAGE 1 </a></h3><br>
-		<h3><a href="image2.html"> IMAGE 2 </a></h3><br>
-		<h3><a href="image3.html"> IMAGE 3 </a></h3>
+		<h3><a href="image1.php"> IMAGE 1 </a></h3><br>
+		<h3><a href="image2.php"> IMAGE 2 </a></h3><br>
+		<h3><a href="image3.php"> IMAGE 3 </a></h3>
 	</main>
 	
 </body>
