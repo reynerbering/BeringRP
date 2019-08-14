035b1c66e63c2e625b8a2f0415e479eb4c3fe14a
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-08-14 14:47:20 +0800

    Update image3.php

--- image3.php
+++ image3.php
@@ -11,6 +11,7 @@
 <center><img src="image3.jpg" alt="image3" width="600" height="850"></center>
 <center>
 <a href="index.php">BACK TO HOME</a>
+<br><a href="index.php">LOG OUT</a>
 </center>
 </body>
 </html>
