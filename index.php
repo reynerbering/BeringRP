cfb6be42fc8be112e33f75f097714dedb2c2558a
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-08-14 09:50:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <body bgcolor="pink">
 <head>
-	<title>HOME</title>
+    <title>
+        LOGIN PAGE
+    </title>
 </head>
 <body>
-	<center><h1>Bering, Reyner Paul's PHOTO GALLERY</h1></center><center>
+<center>
+    <header>
+        <h1>LOG IN</h1>
+        <h2>Enter Your Username and Password Below:</h2>
+    </header>
+</center>
+    <br>
+    <br>
+    <center><main>
+        <tr>
+            <td>Username:</td>
+            <td><input type="text" size=30 name="userid"></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="text" size=30 name="userid"></td>
+        </tr>
+           <a href="home.html"><input type="submit" onclick="return check(this.form)" value="Login"></a>
+    </main></center>
+    <br>
+    <section>
+        <em><center>Note: Never tell anyone your username and password!</center></em>
+    </section>
 
-	<img src="IMAGE.gif">
-	</center>
-	<nav><center>
-		<h3><a href="image1.php"> IMAGE 1 </a><br></h3>
-		<h3><a href="image2.php"> IMAGE 2 </a><br></h3>
-		<h3><a href="image3.php"> IMAGE 3 </a></h3>
-	</center></nav>
+
+    	<a href="RESET.html"><em><h3 align="center">RESET PASSWORD</h3></em></a>
+    	<a href="SIGN UP.html"><h1 align="center">SIGN UP</h1></a>
+
+    <footer>
+        <h2><center>EST.2019, ALL RIGHTS RESERVED</center></h2>
+    </footer>
 </body>
 </html>
