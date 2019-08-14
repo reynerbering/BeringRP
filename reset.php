a2c855b6ddedb712b180001ea80855e5f003158f
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-08-14 14:44:14 +0800

    Update reset.php

--- reset.php
+++ reset.php
@@ -7,7 +7,7 @@
 <body>
 	<center>
 	ENTER EMAIL:<input type="text" size=30 name="userid"><br>
-	<input type="submit" onclick="return check(this.form)" value="RESET PASSWORD">
+	<a href="index.php"><input type="submit" onclick="return check(this.form)" value="RESET PASSWORD"></a>
 
 	</center>
 </body>
