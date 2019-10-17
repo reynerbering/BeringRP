804baadab865006e67ba972fd10c6279ea0d1b02
reynerbering <53499535+reynerbering@users.noreply.github.com>
2019-10-17 23:06:24 +0800

    Add files via upload

---
+++ cornhub/homejs.js
@@ -0,0 +1,14 @@
+var myIndex = 0;
+corn();
+
+function corn() {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(corn, 2000); // Change image every 2 seconds
+}
